5fb99a6bb61c99b3ee5b8cf5be1a6ccd09ba87e6
otueamon <35383168+otueamon@users.noreply.github.com>
2025-01-31 18:43:30 -0800

    Completado para Revision Prac1.c

--- Prac1.c
+++ Prac1.c
@@ -29,14 +29,42 @@ int main()
 //    1011 0000 1011 0000  (valor que se desea)
     a |= (1<<15) | (3<<12) | (1<<7) | (3<<4);
     printf("\na = %X", a);
-    
 //2 BABA
+    //1011 0000 1011 0000
+    //resultado deseado:1011 1010 1011 1010
+    a|= (10<<8) | 10;
+    printf("\na = %X", a);
 //3 BEBE
+    //1011 1010 1011 1010
+    //resultado deseado:1011 1110 1011 1110
+    a|= (1<<10) | (1<<2);
+    printf("\na = %X", a);
 //4 FE00
+    //1011 1110 1011 1110
+    //resultado deseado:1111 1110 0000 0000
+    a |= (1<<14);
+    a &= ~((15<<4) | (7<<1));
+    printf("\na = %X", a);
 //5 FEAA
+    //1111 1110 0000 0000
+    //resultado deseado:1111 1110 1010 1010
+    a |= (10<<4)|10;
+    printf("\na = %X", a);
 //6 0155
+    //1111 1110 0000 0000
+    //resultado deseado:0000 0001 0101 0101
+    a = ~a;
+    printf("\na = %X", a);
 //7 0001
+    //0000 0001 0101 0101
+    //resultado deseado:0000 0000 0000 0001
+    a = 1;
+    printf("\na = %X", a);
 //8 0100
-
+    //0000 0000 0000 0001
+    //resultado deseado:0000 0001 0000 0000
+    a = a<<8;
+    printf("\na = %X", a);
     return 0;
+    //Josue Garcia 1273078
 }
